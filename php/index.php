93c057aa4f0f42f93300881b986cfd955fb248b4
Mahesh Waghmare <mwaghmare7@gmail.com>
2021-04-01 18:58:01 +0530

    Updated the autoload.

--- php/index.php
+++ php/index.php
@@ -1,6 +1,7 @@
 <?php
-if( ! function_exists( 'wp_admin_notices' ) ) :
-    function wp_admin_notices() {
-        return 'Hello from "WP Admin Notices"';
-    }
-endif;
+
+namespace WP_Admin_Notices;
+
+function info() {
+    return 'Hello from "WP Admin Notices"';
+}
